a51d374f5a248243810553111fbd6932615cafc2
Hans-Christian Otto <c.otto@suora.com>
2021-01-12 14:37:49 +0100

    added tsplayground

---
+++ src/tsplayground.ts
@@ -0,0 +1,20 @@
+interface Product {
+  id: string;
+  title: string;
+}
+
+interface ProductTitleProps {
+  product: Product;
+}
+
+
+const ProductTitle = ({ product } : ProductTitleProps) {
+  // const product = props.product
+  // const { product } = props
+  console.log(product.title)
+}
+
+const printProductTitle = ({title, ...rest}: Product) => {
+  console.log(title)
+  console.log(rest.id)
+}
