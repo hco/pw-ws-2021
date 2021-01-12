5688ab7e42e2b1eaba96f0ebae6394e0d61511c8
Hans-Christian Otto <c.otto@suora.com>
2021-01-12 15:07:34 +0100

    cleanup

--- src/tsplayground.ts
+++ src/tsplayground.ts
@@ -1,20 +1,22 @@
-interface Product {
-  id: string;
-  title: string;
-}
+// interface Product {
+//   id: string;
+//   title: string;
+// }
 
-interface ProductTitleProps {
-  product: Product;
-}
+// interface ProductTitleProps {
+//   product: Product;
+// }
 
+// const ProductTitle = ({ product } : ProductTitleProps) {
+//   // const product = props.product
+//   // const { product } = props
+//   console.log(product.title)
+// }
 
-const ProductTitle = ({ product } : ProductTitleProps) {
-  // const product = props.product
-  // const { product } = props
-  console.log(product.title)
-}
+// const printProductTitle = ({title, ...rest}: Product) => {
+//   console.log(title)
+//   console.log(rest.id)
+// }
 
-const printProductTitle = ({title, ...rest}: Product) => {
-  console.log(title)
-  console.log(rest.id)
-}
+export const foo = "bar";
+console.log("foo");
