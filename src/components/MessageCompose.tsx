4dcc608523fe5eaa112a6c3401135aaf7ce2307e
Hans-Christian Otto <c.otto@suora.com>
2021-01-13 10:15:26 +0100

    implemented input in messagecompose

--- src/components/MessageCompose.tsx
+++ src/components/MessageCompose.tsx
@@ -1,11 +1,16 @@
+import {useState} from "react";
+
 const MessageCompose: React.FunctionComponent = () => {
+  const [messageText, setMessageText] = useState("")
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log("message");
+    console.log("message", messageText);
+    setMessageText("")
   };
 
   return (
     <form onSubmit={handleSubmit}>
+      <input value={messageText} onChange={(event) => setMessageText(event.target.value)} />
       <button type="submit">Absenden</button>
     </form>
   );
